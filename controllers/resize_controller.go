6a724bb3647fb2308025d3685a66d60f2a702167
Ewan Valentine <EwanValentine@Ewans-MacBook-Pro.local>
2015-10-16 18:30:20 +0100

    Weird issue

--- controllers/resize_controller.go
+++ controllers/resize_controller.go
@@ -6,22 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mitchellh/goamz/s3"
 	"github.com/nfnt/resize"
-	"image"
 	"image/jpeg"
 	"log"
 	"mime/multipart"
 	"strconv"
 )
-
-type Collection struct {
-	Items []*Resize
-}
-
-type Resize struct {
-	File   string `json:"file" binding:"required"`
-	Width  string `json:"width"`
-	Height string `json:"height"`
-}
 
 type ResizeController struct {
 	bucket *s3.Bucket
@@ -36,16 +25,15 @@ func NewResizeController(bucket *s3.Bucket) *ResizeController {
 // Resize - function for taking images and meta data and resizing
 func (rc *ResizeController) Resize(c *gin.Context) {
 
+	// Don't really know what this bit does to be fair
 	if err := c.Request.ParseMultipartForm(32 << 20); err != nil {
 		log.Fatal(err)
 	}
 
-	for index, element := range c.Request.MultipartForm.File["item"] {
-		fmt.Println(element.Header.Filename)
-	}
-
 	// Get file
 	file, header, err := c.Request.FormFile("file")
+
+	fmt.Println(file)
 
 	// Get original filename
 	filename := header.Filename
@@ -53,15 +41,23 @@ func (rc *ResizeController) Resize(c *gin.Context) {
 		log.Fatal(err)
 	}
 
-	// Get height and width
-	height := c.PostForm("height")
-	width := c.PostForm("width")
+	// Foreach set of dimensions given
+	for _, element := range c.Request.MultipartForm.Value {
 
-	// Crop file
-	finalFile := rc.Crop(height, width, file)
+		// Get height and width
+		height := element[0]
+		width := element[0]
 
-	// Upload file
-	go rc.Upload(filename, finalFile, "image/jpeg", s3.BucketOwnerFull)
+		// Crop file
+		finalFile := rc.Crop(height, width, file)
+
+		fmt.Println(finalFile)
+		fmt.Println(height)
+		fmt.Println(width)
+
+		// Upload file
+		// go rc.Upload(filename, finalFile, "image/jpeg", s3.BucketOwnerFull)
+	}
 
 	c.JSON(200, gin.H{"filename": filename})
 }
@@ -76,14 +72,16 @@ func (rc *ResizeController) Upload(filename string, file []byte, enctype string,
 func (rc *ResizeController) Crop(height string, width string, file multipart.File) []byte {
 
 	// Decode file to image file
-	image, formReg, err := image.Decode(file)
+	image, err := jpeg.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Convert string height and width into 32int
+	// Convert string height and width into 64int
 	h64, err := strconv.ParseUint(height, 10, 32)
 	w64, err := strconv.ParseUint(width, 10, 32)
+
+	// Convert 64int to 32int
 	h := uint(h64)
 	w := uint(w64)
 
