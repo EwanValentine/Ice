1fb8e3731ebfc269afd69ebcd8f182e75ce567d2
Ewan Valentine <EwanValentine@Ewans-MacBook-Pro.local>
2015-10-16 13:46:01 +0100

    It works

--- controllers/resize_controller.go
+++ controllers/resize_controller.go
@@ -1,10 +1,15 @@
 package controllers
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
 	"github.com/nfnt/resize"
+	"image/jpeg"
 	"log"
-	"os"
+	"mime/multipart"
 	"strconv"
 )
 
@@ -15,7 +20,7 @@ func NewResizeController() *ResizeController {
 	return &ResizeController{}
 }
 
-func (rc ResizeController) Resize(c *gin.Context) {
+func (rc *ResizeController) Resize(c *gin.Context) {
 
 	auth, err := aws.EnvAuth()
 
@@ -24,21 +29,45 @@ func (rc ResizeController) Resize(c *gin.Context) {
 	}
 
 	client := s3.New(auth, aws.EUWest)
+	bucket := client.Bucket("20.65twenty.com")
 
 	file, header, err := c.Request.FormFile("file")
 	filename := header.Filename
 
-	height := c.Query("height")
-	width := c.Query("width")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	height := c.PostForm("height")
+	width := c.PostForm("width")
+
+	fmt.Println("Height: " + height)
+	fmt.Println("Width: " + width)
+
+	finalFile := rc.Crop(height, width, file)
+
+	err = bucket.Put(filename, finalFile, "image/jpeg", s3.BucketOwnerFull)
+	if err != nil {
+		panic(err)
+	}
+
+	c.JSON(200, gin.H{"filename": header.Filename})
+}
+
+func (rc *ResizeController) Crop(height string, width string, file multipart.File) []byte {
+
+	image, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
 	h64, err := strconv.ParseUint(height, 10, 32)
 	w64, err := strconv.ParseUint(width, 10, 32)
 	h := uint(h64)
 	w := uint(w64)
 
-	m := resize.Resize(w, h, file, resize.Lanczos3)
+	m := resize.Resize(w, h, image, resize.Lanczos3)
 
-	client.Put(os.Getenv("AWS_BUCKET_NAME"), m)
-
-	c.Data(200, gin.H{"filename": header.Filename})
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, m, nil)
+	return buf.Bytes()
 }
